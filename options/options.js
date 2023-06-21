0ca559a9ac2ae299cc9260b813c63f35022ed4df
Crowdbotics <team@crowdbotics.com>
2023-06-21 23:20:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-cloud-41661",
+  url: "https://rapid-cloud-41661.botics.co",
+  api: "https://rapid-cloud-41661.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
